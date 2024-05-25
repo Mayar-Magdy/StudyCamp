c1a0b2631e9e5d9304ad8256dd93ddcab36cd718
Mayar <maimagdy238@gmail.com>
2024-05-25 13:19:37 +0300

    start user authentication system

--- base/views.py
+++ base/views.py
@@ -16,6 +16,34 @@ from .forms import RoomForm
 #     {'id' : 3 , 'name': 'Ruby' },
 # ]
 
+def loginpage(request):
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            
+            isUser = False
+
+            try :
+                user = User.objects.get(username=username)
+                isUser = True
+            except :
+                messages.error(request  , "User doesn't exist")
+                
+
+            if isUser :
+             user = authenticate(request, username=username, password=password)
+            
+             if user is not None :
+                login(request, user)
+                return redirect('home')
+             else :
+                messages.error(request  , "Username or Password doesn't exist ")
+
+        return render(request, 'base/login.html')
+
+def logoutpage (request):
+            logout(request) 
+            return render(request, 'base/login.html')
 def home(request):
     q= request.GET.get('q') if (request.GET.get('q') != None)  else ''
     rooms = Room.objects.filter(Q(topic__name__icontains=q) | Q(name__icontains=q) | Q(description__icontains=q) )
@@ -30,6 +58,7 @@ def room(request ,pk):
    context = { 'room' : room }
    return render(request, 'room.html',context)
 
+@login_required(login_url='login')
 def createRoom (request):
     form = RoomForm()
 
