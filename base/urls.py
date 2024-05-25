e85ac45fd232b92b34fe5981439a2c95683daf3a
Mayar <maimagdy238@gmail.com>
2024-05-25 13:16:33 +0300

    Complete crud operations and add templates

--- base/urls.py
+++ base/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns =[ 
+    path('logout', views.logoutpage, name="logout"),
+    path('login', views.loginpage, name="login"),
+    path ('', views.home, name="home"),
+    path('room/<str:pk>/',views.room, name="room"),
+    path('createRoom',views.createRoom,name="createRoom"),
+    path('updateRoom/<str:pk>/', views.updateRoom, name="updateRoom"),
+    path('deleteRoom/<str:pk>/',views.deleteRoom,name="deleteRoom"),
 
 ]
